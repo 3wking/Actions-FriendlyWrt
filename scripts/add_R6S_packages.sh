536fdf9d9d407f11af1cd99b6b56d5243fe511be
3wking <43069914+3wking@users.noreply.github.com>
2022-11-17 22:17:08 +0800

    Update add_R6S_packages.sh

--- scripts/add_R6S_packages.sh
+++ scripts/add_R6S_packages.sh
@@ -20,6 +20,7 @@ EOL
     [ -d luci-app-openclash ] && rm -rf luci-app-openclash
     git clone https://github.com/vernesong/OpenClash.git
     mv friendlywrt/package/OpenClash/luci-app-openclash friendlywrt/package/luci-app-openclash
+    rm -rf OpenClash
     [ -d luci-app-openclash ] && echo "luci-app-openclash.....OK"
 })
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> configs/rockchip/01-nanopi
